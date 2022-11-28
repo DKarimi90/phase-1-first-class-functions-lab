0ac13ebb8a22ac2485189d33404968cafdd75697
david <davmwa90@gmail.com>
2022-11-28 19:33:36 +0300

    Created differnt first-class functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// Defining a function and assigning it an anonymous; 
+const returnFirstTwoDrivers = function(drivers) {
+return drivers.slice(0, 2);  
+}
+
+// Declaring a variable and assigning it to an anonymous function; 
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(drivers.length -2); 
+}
+
+const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+// creating a high-order function; 
+function createFareMultiplier(anInteger){
+        return function(fare){
+          return fare*anInteger;
+        };
+    }
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+    return driversToReturn(arrayOfDrivers);
+  };
